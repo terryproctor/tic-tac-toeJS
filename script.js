6625ceb8c9befd85c25472c14f7c23aa7e4c437c
terryproctor <terryproctor2015@outlook.com>
2021-04-28 21:32:01 +0100

    gamebaord

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 const game = (() => {
     //create a new board with empty(null values)
-    const newBoard = () => {
-        let cells = [];
-        function addColumns(rowValue) {
-        for (let i = 1; i<=3; i++) {
-            cells.push(rowValue + i)
-            }
+    const gameBoard = (() => {
+    
+        const newBoard = () => {
+            let cells = [];
+            function addColumns(rowValue) {
+            for (let i = 1; i<=3; i++) {
+                cells.push(rowValue + i)
+                }
+            };
+            let row = ['A','B','C'];
+            row.forEach(row => addColumns(row));
+            let gameboard = {};
+            for (key of cells) {
+                gameboard[key] = null;
+            };
+            return gameboard;
         };
-        let row = ['A','B','C'];
-        row.forEach(row => addColumns(row));
-        let gameboard = {};
-        for (key of cells) {
-            gameboard[key] = null;
-        };
-        return gameboard;
-    };
+    
+        return {newBoard};
+    
+})();
 
+    const player = (name, mark) => {
+        const select = (value) => {
+            if (value !== 'undefined') {
+            return value;
+        }
+        }
+        return {name, mark, select}
+    }
 
 
     return {
-        newBoard,
+        player, gameBoard
     };
 })();
 
-let gameBoard = game.newBoard();
-gameBoard.B2 = 'X';
-console.log(gameBoard);
+
