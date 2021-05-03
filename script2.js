7857808eb4923f39bb463ae4b0bfee958460be6c
terryproctor <terryproctor2015@outlook.com>
2021-05-03 22:04:03 +0100

    players can now selectas a one time event

--- script2.js
+++ script2.js
@@ -1,8 +1,7 @@
 let gameDom = document.getElementById("gameContainer");
 
 const board = (() => {
-    let _gameboard = {
-    }
+    let _gameboard = {};
     createBoard = () => {
         for (i = 1; i <= 9; i++) {
             _gameboard[i] = "";
@@ -12,29 +11,48 @@ const board = (() => {
             gameDom.appendChild(cell);
         }
         return _gameboard;
-    }
+    };
 
     //show updates to the board
     const displayBoard = () => {
         for(element in _gameboard)
             {
-            gameDom.children.namedItem(element).textContent = _gameboard[element]
-            ;
+            gameDom.children.namedItem(element).textContent = 
+            _gameboard[element];
         };
-    }
+    };
     
-
-
 return {createBoard, _gameboard, displayBoard}
 })();
 
+const player = (mark, name) => {
+    this.mark = mark;
+    this.name = name;
+
+    const on = (e) => {
+        if (gb._gameboard[e.target.id] == '') {
+            console.log(gb._gameboard)
+            console.log(e.target.id);
+            console.log(mark);
+            gb._gameboard[e.target.id] = mark;
+            e.target.textContent = mark;
+            console.log(gameDom)
+            gameDom.removeEventListener('click', on)
+            return
+        }
+    }
     
+    const selectCell = () => {
+        gameDom.addEventListener('click', on)
+        }
 
-
-
+        
+return {mark, name, selectCell}
+}
 
 
 const gb = board;
 gb.createBoard();
-gb._gameboard[1] = 'X';
-gb.displayBoard()
+let player1 = player('X', 'Terry');
+let player2 = player('O', 'Barry');
+
