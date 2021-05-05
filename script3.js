53448334ba78ec7fd11fba7bcbe923739268943a
terryproctor <terryproctor2015@outlook.com>
2021-05-05 21:45:25 +0100

    init stage 2

--- script3.js
+++ script3.js
@@ -1,7 +1,8 @@
 const board = (() => {
     let gameDom = document.getElementById("gameContainer");
-
     let _gameboard = {};
+    let _counter = 0;
+    
     createBoard = () => {
         for (i = 1; i <= 9; i++) {
             _gameboard[i] = "";
@@ -13,43 +14,35 @@ const board = (() => {
         return _gameboard;
     };
     
-return {createBoard, _gameboard,}
+    function clickOn() {
+    gameDom.addEventListener('click', turnClick, false);
+    }
+
+    function clickOff() {
+    gameDom.removeEventListener('click', turnClick, false);
+    }
+
+    function turnClick(e) {
+        if (_counter < 9) {
+        console.log(e.target.id);
+        _counter ++;
+        e.target.textContent = currentPlayer.mark;
+        _gameboard[e.target.id] = currentPlayer.mark;
+        console.log(_counter);
+        }
+        clickOff
+    }
+
+    
+return {createBoard, _gameboard, _counter, clickOn, clickOff}
 })();
 //////////////////////////////////////////////////////////////////
 
 const player = (mark, name) => {
     this.mark = mark;
     this.name = name;
-
-    let _selectedVal;
-    
-
-    const on = (e) => {
-        if (gb._gameboard[e.target.id] == '') {
-            e.preventDefault();
-            
-            gb._gameboard[e.target.id] = mark;
-            e.target.textContent = mark;
-    
-            gameDom.removeEventListener('click', on)
-            
-            return
-        }
-    }
-    
-    const selectCell = () => {
-        _selectedVal = gameDom.addEventListener('click', on)
-        console.log('Hi')
-        gb.counter++
-        return
-        }
-
-    const getSelected = () => {
-        return _selectedVal
-      }
-
         
-return {mark, name, selectCell, getSelected}
+return {mark, name}
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -58,3 +51,5 @@ const gb = board;
 gb.createBoard();
 let player1 = player('X', 'Terry');
 let player2 = player('O', 'Barry');
+let currentPlayer = player1;
+gb.clickOn()
